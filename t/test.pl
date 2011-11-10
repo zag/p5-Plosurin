f11061fc13a9b630102bfc7daef65f85bfd1e3bc
Aliaksandr Zahatski <zahatski@gmail.com>
2011-11-10 18:42:42 +0300

    action

--- t/test.pl
+++ t/test.pl
@@ -1,4 +1,24 @@
 #!/usr/bin/perl 
+package TmplFile;
+use strict;
+use warnings;
+use Data::Dumper;
+sub new {
+   my $class = shift;
+   $class = ref $class if ref $class;
+   my $self = bless( {@_}, $class );
+   $self;
+}
+
+sub template {
+    my ($self , $a) = @_;
+#    return {%a}
+#    diag Dumper
+    die Dumper($a,$self);
+}
+
+1;
+package main;
 use strict;
 use warnings;
 use feature qw/say/;
@@ -14,24 +34,47 @@ my $t = <<'TEXT';
  * @param test Test file
  * @param test1 Test file
  **/
-{template}
+{template .test1}
+
+asdasd asd
 {/template}
+
+/**
+ * Test1
+ * Test2
+ * @param test Test file
+ * @param test1 Test file
+ **/
+{template .test}
+       
+asdasd asd
+{/template}
+
+
 TEXT
+our $file = 'TST';
 my $r = qr{
     <namespace>
-    <header>
-
+    <[Templates]>+
+    <rule: Templates>  <header>  <template>
     <rule: namespace> \{namespace <id>\} \n+
     <rule: id>  \w+
-    <rule: header> \/\*+\n <[h_line]>+ \*+\/ 
-    <rule: h_line> <h_params> | <h_comment> 
+    <rule: header> \/\*+\n (?: <h_params> | <h_comment> )+ \*+\/ 
     <rule: h_comment> \s+\* <raw_str>
     <rule: raw_str> [^@\n]+$
     <rule: h_params> \s+\* \@param <id> <raw_str>
+    <rule: template><start_from= matchline><start_template>
+            <raw_template=(.*?)><stop_template><stop_at= matchline>
+            (?{  Dumper(\%MATCH); $MATCH{1} =1; })
+    <rule: raw_template>  .*?
+    <rule: start_template> \{template <name=(\.\w+)>\} 
+    | <matchline><fatal:(?{say "Bad template definition at $file line $MATCH{matchline} : $CONTEXT" })>
+    <rule: stop_template>  \{\/template\}
 }xms;
 
+
 say "ok";
-if ( $t =~ $r ) {
+if ( $t =~ $r->with_actions(TmplFile->new(file=>"Test")) ) {
 
     sub strip {
         my $ref = shift;
@@ -49,8 +92,9 @@ if ( $t =~ $r ) {
         $ref;
     }
 #    say "OK: " .  pos($t);
-#    say Dumper strip( {%/} );
-     say Dumper strip({%/});
+#    say Dumper  {%/} ;
+    say Dumper strip({%/});
+    say {*STDERR} $_ for @!;
 }
 else {
     say "Bad string";
